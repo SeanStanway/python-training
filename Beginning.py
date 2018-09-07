6b5a585621af1951b08cd5d756cad25d2895ca63
Sean Stanway <sean.stanway@hotmail.co.uk>
2018-09-07 10:53:54 +0100

    New line function altered
    Similar to print_function alteration, doesn't need to be 'printed' outside the function

--- Beginning.py
+++ Beginning.py
@@ -1,14 +1,14 @@
 def print_function(exampleName, content):
     print('{} {}'.format(exampleName, content))
 def newLine():
-    return ''
+    print('')
 
 def my_function():
     a = 2
     return a
 print_function("Function:", my_function())
 
-print(newLine())
+newLine()
 
 b = 5
 c = 7
@@ -17,43 +17,43 @@ if b > c:
 else:
     print_function('If Statement:','c is more than b')
 
-print(newLine())
+newLine()
 
 import math
 newMath = math.sqrt(16)
 print_function('Math Import:', newMath)
 
-print(newLine())
+newLine()
 
 int_list = [1, 2, 3]
 string_list = ['test', '123']
 nested_list = [['nest', 'two'], [100, 200]]
 print_function('Int List:', int_list)
 print_function('Int List, Single Value:', int_list[1])
-print(newLine())
+newLine()
 print_function('String List:', string_list)
 print_function('String List, Negative Counting ([-2]):',string_list[-2])
-print(newLine())
+newLine()
 print_function('Nested List:', nested_list)
 print_function('Nested List, Single Value:', nested_list[0][1])
 nested_list[0][1] = 'New Stuff'
 print_function('Nested List, Mutated Value', nested_list)
 int_list.insert(3, 4)
-print(newLine())
+newLine()
 print_function('Int List, Insert:', int_list)
 int_list.remove(2)
 print_function('Int List, Remove', int_list)
 print_function('Int List, Len(gth):', len(int_list))
 print_function('String List, Index Location:', string_list.index('123'))
 
-print(newLine())
+newLine()
 
 print('-----For Loop')
 for element in int_list:
     print (element)
 print('-----For Loop END')
 
-print(newLine())
+newLine()
 
 newTuple = ('127.0.0.1', '443')
 print_function('Tuple (Fixed list):', newTuple)
