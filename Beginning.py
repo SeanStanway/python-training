5c5f1cc6f66660351e9352f28c9f311f6bf89d8a
Sean Stanway <sean.stanway@hotmail.co.uk>
2018-09-07 10:09:25 +0100

    Master file created
    Started python tutorial coding

---
+++ Beginning.py
@@ -0,0 +1,16 @@
+def my_function():
+    a = 2
+    return a
+print('{} {}'.format("Function:", my_function()))
+
+b = 5
+c = 7
+if b > c:
+    print('b is more than c')
+else:
+    print('c is more than b')
+
+int_list = [1, 2, 3]
+string_list = ['test', '123']
+print(int_list[0])
+print(string_list[-2])
